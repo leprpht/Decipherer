c0b1597b52741a3a25bdb11c0ec34af14393d9fe
Artur Gorbachev <artur.gorbachev@microsoft.wsei.edu.pl>
2025-11-22 20:03:49 +0100

    Refactor caesar cipher to accept Polish characters

--- src/ciphers/caesar.ts
+++ src/ciphers/caesar.ts
@@ -2,20 +2,23 @@ import type { Cipher } from "../types/cipher";
 
 export class CaesarCipher implements Cipher {
     id: number = 1;
+    alphabet: string = 'AĄBCĆDEĘFGHIJKLŁMNOÓPQRSŚTUVWXYZŹ';
 
     encode(text: string, key: number = 3): string {
-        return text.split('').map(char => {
-            if (char >= 'a' && char <= 'z') {
-                return String.fromCharCode((char.charCodeAt(0) - 97 + key) % 26 + 97);
-            } else if (char >= 'A' && char <= 'Z') {
-                return String.fromCharCode((char.charCodeAt(0) - 65 + key) % 26 + 65);
-            } else {
-                return char;
-            }
+        return text.toUpperCase().split('').map(char => {
+            const index = this.alphabet.indexOf(char);
+            if (index === -1) return char;
+            const newIndex = (index + key) % this.alphabet.length;
+            return this.alphabet[newIndex];
         }).join('');
     }
 
     decode(text: string, key: number = 3): string {
-        return this.encode(text, 26 - key);
+        return text.toUpperCase().split('').map(char => {
+            const index = this.alphabet.indexOf(char);
+            if (index === -1) return char;
+            const newIndex = (index - key + this.alphabet.length) % this.alphabet.length;
+            return this.alphabet[newIndex];
+        }).join('');
     }
 }
